06e7bc2217cd3470d9eaa4ab44c59cf47464bad2
freakGE <esebua154@gmail.com>
2022-11-05 16:06:35 +0400

    Added lazy load for images

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,33 @@
-const hamburger = document.querySelector('#hamburger');
-const header = document.querySelector('.header-container');
+const hamburger = document.querySelector("#hamburger");
+const header = document.querySelector(".header-container");
 
-hamburger.addEventListener('click', () => {
-    header.classList.toggle('show');
-    hamburger.classList.toggle('window');
-})
+hamburger.addEventListener("click", () => {
+  header.classList.toggle("show");
+  hamburger.classList.toggle("window");
+});
+
+const preloadImage = img => {
+  const src = img.getAttribute("data-img");
+  if (!src) return;
+
+  img.src = src;
+};
+
+const images = document.querySelectorAll("[data-img]");
+
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 300px 0px",
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    preloadImage(entry.target);
+    imgObserver.unobserve(entry.target);
+  });
+}, imgOptions);
+
+images.forEach(image => {
+  imgObserver.observe(image);
+});
